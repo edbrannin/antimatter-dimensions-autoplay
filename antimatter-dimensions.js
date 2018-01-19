f76fa12f94b9ed0a80ad7650e6b29490bd45db7d
Ed Brannin <edbrannin@gmail.com>
2018-01-19 16:51:56 -0500

    Updates

--- antimatter-dimensions.js
+++ antimatter-dimensions.js
@@ -1,20 +1,27 @@
 interval = setInterval((state) => {
-  console.log('Max all');
-  $('#maxall').click();
+  maxAll = () => {
+    console.log('Max all');
+    $('#maxall').click();
+  };
+  maxAll();
   sac_text = $('#sacrifice').text().match(/\(([0-9]+.*)x\)/)[1];
   sac = Number(sac_text);
   if (state.last_sac === undefined) {
     state.last_sac = sac
   }
-  if (sac > 10 && (sac - state.last_sac) < 1) {
-    // TODO check if number is increasing really fast and hold off
-    console.log('Sacrifice for', sac);
-    $('#sacrifice').click();
-    $('#maxall').click();
+  if (sac > 10) {
+    if ((sac - state.last_sac) < 1 || sac > 500) {
+      console.log('Sacrifice for', sac);
+      $('#sacrifice').click();
+      setTimeout(maxAll, 100);
+    } else {
+      console.log("Increasing too fast:", sac - state.last_sac, "since last tick");
+    }
   }
   if ($('#softReset.storebtn').text() ===  "Reset the game for a new Dimension") {
     $('#softReset.storebtn').click();
-    $('#maxall').click();
+    setTimeout(maxAll, 100);
   }
-}, 2500, {});
+  // TODO Also reset if #confirmation isn't present
+}, 500, {});
 
