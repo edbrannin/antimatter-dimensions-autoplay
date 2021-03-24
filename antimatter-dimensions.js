87b5ab49ea9d04601865f750a42b7806cd0f170f
Ed Brannin <edbrannin@gmail.com>
2021-03-24 11:52:21 -0400

    Saving old code

--- antimatter-dimensions.js
+++ antimatter-dimensions.js
@@ -1,9 +1,15 @@
-clearInterval(interval);
-interval = setInterval((state) => {
-  soft_resets = true;
+autoPlayer = 
+(state) => {
+  verbose = false;
+  log = (...args) => {
+    if (verbose) {
+      console.log(...args);
+    }
+  };
+  soft_resets = false;
   galaxy_resets = false;
   maxAll = () => {
-    console.log('Max all');
+    log('Max all');
     if (true) {
       $('#maxall').click();
     } else {
@@ -17,21 +23,24 @@ interval = setInterval((state) => {
   // Reset, if possible
   $('#bigcrunch').click();
   maxAll();
-  sac_text = $('#sacrifice').text().match(/\(([0-9]+.*)x\)/)[1];
-  sac = Number(sac_text);
-  if (state.last_sac === undefined) {
+  // Sacrifice
+  if ($('#confirmation')[0].checked) {
+    sac_text = $('#sacrifice').text().match(/\(([0-9]+.*)x\)/)[1];
+    sac = Number(sac_text);
+    if (state.last_sac === undefined) {
+      state.last_sac = sac
+    }
+    if (sac > 10) {
+      if ((sac - state.last_sac) < 1 || sac > 500) {
+        log('Sacrifice for', sac);
+        $('#sacrifice').click();
+        setTimeout(maxAll, 100);
+      } else {
+        log("Increasing too fast:", sac - state.last_sac, "since last tick");
+      }
+    }
     state.last_sac = sac
   }
-  if (sac > 10) {
-    if ((sac - state.last_sac) < 1 || sac > 500) {
-      console.log('Sacrifice for', sac);
-      $('#sacrifice').click();
-      setTimeout(maxAll, 100);
-    } else {
-      console.log("Increasing too fast:", sac - state.last_sac, "since last tick");
-    }
-  }
-  state.last_sac = sac
   if ($('#softReset.storebtn').text() === 'Reset the game for a new Dimension' || $('#confirmation').css('display') ===  'none') {
     $('#softReset.storebtn').click();
     setTimeout(maxAll, 100);
@@ -42,4 +51,29 @@ interval = setInterval((state) => {
     $('#softReset.storebtn').click();
   }
   // TODO Also reset if #confirmation isn't present
-}, 500, {});
+};
+
+// Early-game
+if (typeof interval !== 'undefined') {
+  clearInterval(interval);
+}
+interval = setInterval(autoPlayer, 500, {});
+
+// Faster
+if (typeof interval !== 'undefined') {
+  clearInterval(interval);
+}
+interval = setInterval(autoPlayer, 250, {});
+
+
+// Even faster
+if (typeof interval !== 'undefined') {
+  clearInterval(interval);
+}
+interval = setInterval(autoPlayer, 100, {});
+
+// Broke Infinity
+if (typeof interval !== 'undefined') {
+  clearInterval(interval);
+}
+interval = setInterval(() => $('#maxall').click(), 50);
